4b1e97ab08beade875e1aa97467adb8cff74c965
vittfiles <vittfiles@gmail.com>
2025-01-06 14:33:08 -0300

    first commit

---
+++ src/test/java/com/vittfiles/Alura_Oracle_Challenge_java_Spring/AluraOracleChallengeJavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vittfiles.Alura_Oracle_Challenge_java_Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AluraOracleChallengeJavaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
